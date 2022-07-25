3a7e2aeca52ef86d266f715174a8179d24ef7daf
AHMAD-KHAIRI <ahmadkhairi_hamzah85@yahoo.com>
2022-07-26 01:09:02 +0800

    learned how to ask the user for their name using input function, saving the user name in a variable and lastly how to say hello to the user using print function

--- hello.py
+++ hello.py
@@ -1,1 +1,19 @@
+
+# Ask user for their name
+name = input("What's your name? ")
+
+# Remove whitespace from str
+name = name.strip()
+
+# Say hello to user (multiple ways to do it)
+print(f"hello, {name}")
+# print("hello,", name)
+# print("hello, " + name)
+# print("hello,")
+# print(name)
+
+# first python code with print function
 print("hello, world")
+
+
+
