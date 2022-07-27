95685412500b06869cf89eaadc06583e84c2c9d7
AHMAD-KHAIRI <ahmadkhairi_hamzah85@yahoo.com>
2022-07-27 23:09:28 +0800

    learned how to use string methods such as strip(), capitalize(), title(), split() and also combining multiple methods in a single line of code

--- hello.py
+++ hello.py
@@ -1,19 +1,32 @@
 
-# Ask user for their name
-name = input("What's your name? ")
+# Ask user for their name and combine with methods to remove whitespace and capitalize first letter of every word
+name = input("What's your name? ").strip().title()
 
-# Remove whitespace from str
-name = name.strip()
+# Split user's name into first name and last name
+first, last = name.split(" ")
+
+# Combining methods: remove whitespace and capitalize first letter of every word
+# name = name.strip().title()
+
+# Remove whitespace from str, remove whitespaces to the left and right of the str
+# name = name.strip()
+
+# Capitalize user's name, capitalizing the first letter of the str
+# name = name.capitalize()
+
+# Capitalize user's name method #2 using title
+# name = name.title()
 
 # Say hello to user (multiple ways to do it)
-print(f"hello, {name}")
+print(f"hello, {first}")
+# print(f"hello, {name}")
 # print("hello,", name)
 # print("hello, " + name)
 # print("hello,")
 # print(name)
 
 # first python code with print function
-print("hello, world")
+# print("hello, world")
 
 
 
