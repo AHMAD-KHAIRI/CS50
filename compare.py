a1f299bbac3155880e0da36d3da6531f473396fd
AHMAD-KHAIRI <ahmadkhairi_hamzah85@yahoo.com>
2022-07-29 16:52:48 +0800

    first commit

---
+++ compare.py
@@ -0,0 +1,11 @@
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+if x < y:
+    print("x is less than y")
+if x > y:
+    print("x is more than y")
+if x == y:
+    print("x is equal to y")
+
+
