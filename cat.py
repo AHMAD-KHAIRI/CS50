b54223a60d1f6125d097a227aa6ee123bd9d1f99
AHMAD-KHAIRI <ahmadkhairi_hamzah85@yahoo.com>
2022-07-30 14:29:45 +0800

    learned about while loop

--- cat.py
+++ cat.py
@@ -1,2 +1,15 @@
 # CS50P - Lecture 2 - Loops
 # Made by AK on 30072022
+
+# print("Meow")
+# print("Meow")
+# print("Meow")
+
+# Introduction of while loop
+# First we initialize how many times we want the loop to repeat
+i = 3 
+# Next, do the while loop and check if i is not equal to 0. If true then print meow and move down. If false then stop
+while i != 0:
+    print("Meow")
+    # After printing meow, do the calculation (e.g. i = 3 - 1 = 2) and return to while loop and check again
+    i = i - 1
