347f912a4a53babe17ffdcc2a7ed459a7c89c229
AHMAD-KHAIRI <ahmadkhairi_hamzah85@yahoo.com>
2022-07-30 14:47:25 +0800

    added some alternative solution using while loop

--- cat.py
+++ cat.py
@@ -5,11 +5,22 @@
 # print("Meow")
 # print("Meow")
 
+# Ask the user how many times to repeat printing meow
+# n = int(input("How many times? "))
+# i = n
+
 # Introduction of while loop
 # First we initialize how many times we want the loop to repeat
-i = 3 
-# Next, do the while loop and check if i is not equal to 0. If true then print meow and move down. If false then stop
-while i != 0:
+# i = 3
+# # Next, do the while loop and check if i is not equal to 0. If true then print meow and move down. If false then stop
+# while i != 0:
+#     print("Meow")
+#     # After printing meow, do the calculation (e.g. i = 3 - 1 = 2) and return to while loop and check again
+#     # i = i - 1
+#     i -= 1
+
+# or we do it this way by convention
+i = 0
+while i < 3:
     print("Meow")
-    # After printing meow, do the calculation (e.g. i = 3 - 1 = 2) and return to while loop and check again
-    i = i - 1
+    i += 1
