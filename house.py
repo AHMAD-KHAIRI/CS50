50c8ed1ad57685ff4c3d15719175bd19d4fb031d
AHMAD-KHAIRI <ahmadkhairi_hamzah85@yahoo.com>
2022-07-30 12:45:42 +0800

    Learned about match conditional statement as an alternative to if statement

--- house.py
+++ house.py
@@ -3,13 +3,21 @@
 
 name = input("What's your name? ")
 
-if name == "Harry":
-    print("Gryffindor")
-elif name == "Hermione":
-    print("Gryffindor")
-elif name == "Ron":
-    print("Gryffindor")
-elif name == "Draco":
-    print("Slytherin")
-else:
-    print("Who?")
+# use if elif else statements
+# if name == "Harry" or "Hermione" or "Ron":
+#     print("Gryffindor")
+# elif name == "Draco":
+#     print("Slytherin")
+# else:
+#     print("Who?")
+
+# Introduction of match conditional statement as alternative to if conditional statement
+match name:
+    case "Harry"| "Hermione" | "Ron":
+    # case "Hermione":
+    # case "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who?")
