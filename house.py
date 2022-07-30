af39e13c53496bc2b52f3bc63c75daf79d710d77
AHMAD-KHAIRI <ahmadkhairi_hamzah85@yahoo.com>
2022-07-30 12:38:20 +0800

    Added fourth exercise house.py where it prompts for the user name and outputs the house in Harry Potter

---
+++ house.py
@@ -0,0 +1,15 @@
+#  fourth exercise in CS50P - Lecture 1 - Conditionals
+# prompts user for their name and outputs their house in Harry Potter
+
+name = input("What's your name? ")
+
+if name == "Harry":
+    print("Gryffindor")
+elif name == "Hermione":
+    print("Gryffindor")
+elif name == "Ron":
+    print("Gryffindor")
+elif name == "Draco":
+    print("Slytherin")
+else:
+    print("Who?")
