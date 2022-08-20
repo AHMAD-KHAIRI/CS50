c550b14e466d44cfa022e8a67cf741dcbe8c2a0b
AHMAD-KHAIRI <ahmadkhairi_hamzah85@yahoo.com>
2022-08-20 17:53:03 +0800

    first commit

---
+++ generate.py
@@ -0,0 +1,8 @@
+# create a python program that simulates the flipping of a coin
+
+# first import the random library
+import random
+
+# random.choice(seq): Return a random element from the non-empty sequence seq.
+# create a list: "heads" or "tails" and save it in a variable
+coin = random.choice(["head", "tails"])
