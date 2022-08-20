e0a65891c6d549cb05884c7b881d5d53a88d39e5
AHMAD-KHAIRI <ahmadkhairi_hamzah85@yahoo.com>
2022-08-20 18:00:20 +0800

    imported random library and created a program that simulates the flipping of a coin

--- generate.py
+++ generate.py
@@ -1,8 +1,15 @@
 # create a python program that simulates the flipping of a coin
 
 # first import the random library
-import random
+# import random
+
+# if we don't want to add the random library, we can use from and import only the function that we want
+# this means we don't need to call random.choice but just call the function choice
+from random import choice
 
 # random.choice(seq): Return a random element from the non-empty sequence seq.
 # create a list: "heads" or "tails" and save it in a variable
-coin = random.choice(["head", "tails"])
+# coin = random.choice(["head", "tails"])
+coin = choice(["head", "tails"])
+
+print(coin)
